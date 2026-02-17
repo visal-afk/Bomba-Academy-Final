17aa9b1059b589ad6b058991455689df79c1bd1b
Vusal Movsumlu <vusalmovsum@outlook.com>
2026-02-17 23:33:38 +0400

    Db problemler hel olunmus

--- Bomba-Academy.Domain/DTOs/AuditorimInfoDto.cs
+++ Bomba-Academy.Domain/DTOs/AuditorimInfoDto.cs
@@ -3,6 +3,8 @@
 public class AuditorimInfoDto
 {
     public int Id { get; set; }
-    public string Name { get; set; }
-    
+    public string? Name { get; set; }
+    public string? GroupName { get; set; }
+    public int? Course { get; set; }
+
 }
